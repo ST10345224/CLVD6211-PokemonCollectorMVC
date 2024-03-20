1850b103ad691ec6d6d02f6b1a7427b557618796
ST10345224 <keval.balgobind@gmail.com>
2024-03-20 22:42:56 +0200

    Add project files.

---
+++ Areas/Identity/Data/PokemonCollectorMVCContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokemonCollectorMVC.Areas.Identity.Data;
+
+namespace PokemonCollectorMVC.Data;
+
+public class PokemonCollectorMVCContext : IdentityDbContext<PokemonCollectorMVCUser>
+{
+    public PokemonCollectorMVCContext(DbContextOptions<PokemonCollectorMVCContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplciationUserEntityConfiguration());
+    }
+}
+
+public class ApplciationUserEntityConfiguration : IEntityTypeConfiguration<PokemonCollectorMVCUser>
+{
+    public void Configure(EntityTypeBuilder<PokemonCollectorMVCUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(100);
+        builder.Property(x => x.LastName).HasMaxLength(100);
+
+    }
+}
---
+++ Models/TCG.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonCollectorMVC.Models
+{
+    public class TCG
+    {
+        public int EditionId { get; set; }
+        public string CardSeries { get; set; }
+        public string CardName { get; set; }
+        public string CardType { get; set; }
+    }
+}
