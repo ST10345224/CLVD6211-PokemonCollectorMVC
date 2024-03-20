a57953a8ad8eff45ac17c877d62ca0f9cbf863f2
ST10345224 <keval.balgobind@gmail.com>
2024-03-20 22:55:38 +0200

    TCG DB Migration

--- Migrations/20240320205425_TCGMigration.Designer.cs
+++ Migrations/20240320205425_TCGMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PokemonCollectorMVC.Data;
 
@@ -11,9 +12,11 @@ using PokemonCollectorMVC.Data;
 namespace PokemonCollectorMVC.Migrations
 {
     [DbContext(typeof(PokemonCollectorMVCContext))]
-    partial class PokemonCollectorMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20240320205425_TCGMigration")]
+    partial class TCGMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
